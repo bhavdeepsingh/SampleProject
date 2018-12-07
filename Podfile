040402f8dec65ff03913b555121e49d115149bf8
bhasingh <bhasingh@paypal.com>
2018-12-06 21:51:03 -0800

    Sample Project with Podfile

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+
+source 'https://github.com/bhavdeepsingh/SamplePodspecs'
+project 'SampleProject'
+
+target 'SampleProject' do
+ pod 'MyDependency', '1.0.0+code'
+end
---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Singh,Bhavdeep on 12/6/18.
+//  Copyright © 2018 Singh,Bhavdeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
